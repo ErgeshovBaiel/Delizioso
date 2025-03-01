d9becd04c04f9b9a05a3457117cbd8f6a24460c2
Baiel User <baiel@example.com>
2025-03-01 10:06:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 class="text-3xl flex items-center justify-center mt-15 font-bold">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
