4e023d9bdc205d5ff095380aac17da1ae2ad686d
Baiel User <baiel@example.com>
2025-03-04 10:47:57 +0600

    category

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { supabase } from './supabase'
 
 const App = () => {
+  const getData = async () => {
+    let { data: category, error } = await supabase.from('category').select('*')
+    console.log(category);
+    
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div className="text-center text-5xl mt-25 text-green-600 font-medium">Hello World</div>
+    <div className='text-center text-5xl mt-25 text-green-600 font-medium'>
+      <img
+        src='https://drive.google.com/file/d/103iL6RPrqReDP6KoIn8iTxBKtwXXiEgu/view?usp=drive_link'
+        alt='lll'
+      />
+      Hello World
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+// https://dmlxwyvbgetqluvrorpj.supabase.co
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtbHh3eXZiZ2V0cWx1dnJvcnBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwNTc2MzMsImV4cCI6MjA1NjYzMzYzM30.Fy8AP6Jz19ViKu1PB3HkFM1Ih0ti4auNML-TUwB2flY
+
+
+
+/*import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://dmlxwyvbgetqluvrorpj.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)*/ 
